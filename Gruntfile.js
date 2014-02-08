80fea7510bd30fda6b355e7977e180ddd92c4bf9
Ricardo Havill <rhavill@progresstesting.com>
2014-02-08 16:32:06 -0500

    Added concat task to grung config and a concat example.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,20 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        //src: ['src/**/*.js'],
+        src: ['*.js'],
+        // the location of the resulting JS file
+        //dest: 'dist/<%= pkg.name %>.js'
+        dest: '<%= pkg.name %>.js'
+      }
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -16,8 +30,9 @@ module.exports = function(grunt) {
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concat','uglify']);
 
 };
---
+++ concat-test.js
@@ -0,0 +1,1 @@
+console.log("Hello from concat-test.js.");
