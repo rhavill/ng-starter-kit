924fd975a6937c9d98a0c3da16a4681448ea3d9c
Ricardo Havill <rhavill@progresstesting.com>
2014-02-09 16:53:46 -0500

    Added grunt-html2js task to convert angular template files into js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
     build_dir: 'build',
     compile_dir: 'bin',
+    garbage_files: {
+      js: [ 'build/templates-app.js'],
+      jsunit: [ 'src/**/*.spec.js' ],
+
+      atpl: [ 'src/app/**/*.tpl.html' ],
+      ctpl: [ 'src/common/**/*.tpl.html' ],
+
+      html: [ 'src/index.html' ]
+    },
     app_files: {
       js: [ 'src/**/*.js', '!src/**/*.spec.js', '!src/assets/**/*.js' ],
       jsunit: [ 'src/**/*.spec.js' ],
@@ -13,22 +22,28 @@ module.exports = function(grunt) {
 
       html: [ 'src/index.html' ]
     },
-  vendor_files: {
-    js: [
-      'vendor/angular/angular.js',
-      'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
-      'vendor/angular-ui-router/release/angular-ui-router.js',
-      'vendor/angular-ui/common/module.js',
-      'vendor/angular-ui/modules/directives/route/route.js',
-      'vendor/angular-ui/modules/directives/showhide/showhide.js'
-    ],
-    css: [
-      'vendor/bootstrap/dist/css/bootstrap.css',
-      'vendor/bootstrap/dist/css/bootstrap-theme.css'
-    ],
-    assets: [
-    ]
-  },
+    vendor_files: {
+      js: [
+        'vendor/angular/angular.js',
+        'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
+        'vendor/angular-ui-router/release/angular-ui-router.js',
+        'vendor/angular-ui/common/module.js',
+        'vendor/angular-ui/modules/directives/route/route.js',
+        'vendor/angular-ui/modules/directives/showhide/showhide.js'
+      ],
+      css: [
+        'vendor/bootstrap/dist/css/bootstrap.css',
+        'vendor/bootstrap/dist/css/bootstrap-theme.css'
+      ],
+      assets: [
+      ]
+    },
+    template_js_files: {
+      template_js_files: [
+        'templates-app.js',
+        'templates-common.js'
+      ]
+    },
     pkg: grunt.file.readJSON('package.json'),
     index: {
       src: '<%= app_files.html %>',  // source template file
@@ -78,12 +93,45 @@ module.exports = function(grunt) {
         src: 'uglify-test.js',
         dest: 'uglify-test.min.js'
       }
-    }
+    },
+    html2js: {
+      /**
+       * These are the templates from `src/app`.
+       */
+      app: {
+        options: {
+          base: 'src/app'
+        },
+        src: [ '<%= app_files.atpl %>' ],
+        dest: '<%= build_dir %>/templates-app.js'
+      },
+
+      /**
+       * These are the templates from `src/common`.
+       */
+      common: {
+        options: {
+          base: 'src/common'
+        },
+        src: [ '<%= app_files.ctpl %>' ],
+        dest: '<%= build_dir %>/templates-common.js'
+      }
+    },
   });
+
+  function filterForJS ( files ) {
+    return files.filter( function ( file ) {
+      return file.match( /\.js$/ );
+    });
+  }
 
   grunt.registerTask( "index", "Generate index.html depending on configuration", function() {
     var conf = grunt.config('index'),
         tmpl = grunt.file.read(conf.src);
+    var jsFiles = filterForJS( this.filesSrc ).map( function ( file ) {
+      console.log(file.replace( dirRE, '' ));
+      return file.replace( dirRE, '' );
+    });
 
     grunt.file.write(conf.dest, grunt.template.process(tmpl));
 
@@ -94,8 +142,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-html2js');
 
   // Default task(s).
-  grunt.registerTask('default', ['index','copy','concat','uglify']);
+  grunt.registerTask('default', ['copy','concat','uglify','html2js','index']);
 
 };
