9d55218195b9e79dc572676c17cab5a819417788
Ricardo Havill <rhavill@progresstesting.com>
2014-02-10 22:37:10 -0500

    Set active navigation link according to current route.

--- src/app/app.js
+++ src/app/app.js
@@ -13,8 +13,15 @@ angular.module('ngStarterKit', [
       $urlRouterProvider.otherwise('/home');
     })
 
-    .run(function run() {
-    })
+    .run(['$rootScope', '$state', '$stateParams',
+      function ($rootScope, $state, $stateParams) {
+        // It's very handy to add references to $state and $stateParams to the $rootScope
+        // so that you can access them from any scope within your applications.For example,
+        // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+        // to active whenever 'contacts.list' or one of its decendents is active.
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+      }])
 
     .controller('AppCtrl', function AppCtrl($scope, $location, $state, login) {
       $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
